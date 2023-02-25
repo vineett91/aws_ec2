a1ec4121206c89beb1e4a350ffa249e3cd0d1523
vineett91 <95580297+vineett91@users.noreply.github.com>
2023-02-25 17:25:11 +1300

    Update iac_ec2_demo.tf

--- iac_ec2_demo.tf
+++ iac_ec2_demo.tf
@@ -5,9 +5,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-00068cd7555f543d5"
+  ami           = "ami-0a8b4cd432b1c3063"
   instance_type = "t2.micro"
-
+  key_name      = "awsvmkey"
+    
   tags = {
     Name = "aws_demo"
   }
