a5f81647aadfd86b3962d46de888c2ffc45d4669
vineett91 <95580297+vineett91@users.noreply.github.com>
2023-02-25 16:51:47 +1300

    Update iac_ec2_demo.tf

--- iac_ec2_demo.tf
+++ iac_ec2_demo.tf
@@ -1,18 +1,14 @@
-#provider.tf
-
-provider "aws"{
-region = "us-east-1"
-access_key = "AKIAVI7OX3YFC35MSP67"
-secret_key = "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"
+provider "aws" {
+    access_key = "AKIAVI7OX3YFC35MSP67"
+    secret_key = "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"
+    region = "us-east-1"
 }
 
- #ec2.tf
-
-resource "aws_instance" "demo_ec2" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "demo" {
+  ami           = "ami-00068cd7555f543d5"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demo for jenkins terraform"
+    Name = "aws_demo"
   }
 }
